241f7e7e22c611bd7cd6fd9afeb007adc68ce1ce
Elias Andre Da Silva <87732458+Elias-WTS@users.noreply.github.com>
2022-09-10 22:30:05 -0300

    :sparkles: [FEAT]: Add IfTest, WhileTest e M_F_App

---
+++ chapter_01/classes/MyFirstApp.java
@@ -0,0 +1,10 @@
+package headfirstjava.chapter_01.classes;
+
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("Aprendendo Java!");
+
+    }
+
+}
